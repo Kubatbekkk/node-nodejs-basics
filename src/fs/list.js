5a63c6ccd8e28bd0df2f1d61227a93815b369523
Kubatbekkk <kubineccastro@gmail.com>
2023-11-17 11:12:32 +0600

    next commit

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,25 @@
+import * as fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const list = async () => {
-    // Write your code here 
+  // Write your code here
+  const sourceDir = path.join(__dirname, "files");
+
+  if (!fs.existsSync(sourceDir)) {
+    console.error("FS operation failed");
+  } else {
+    fs.readdir(sourceDir, (err, files) => {
+      if (err) {
+        console.error(err);
+      } else {
+        console.log(files);
+      }
+    });
+  }
 };
 
 await list();
