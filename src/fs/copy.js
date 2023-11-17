52f054dfb93b0706255b249adc0659721a4c83bb
Kubatbekkk <kubineccastro@gmail.com>
2023-11-17 11:00:12 +0600

    chore: some tasks have been done

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,25 @@
+import * as fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const copy = async () => {
-    // Write your code here 
+  const sourcePath = path.join(__dirname, "files");
+  const destinationPath = path.join(__dirname, "files_copy");
+
+  if (!fs.existsSync(sourcePath) || fs.existsSync(destinationPath)) {
+    console.error("FS Error file_copy exists or files doesn't exist");
+  } else {
+    fs.cp(sourcePath, destinationPath, { recursive: true }, (err) => {
+      if (err) {
+        console.error(err.message);
+      } else {
+        console.log("FS copy success");
+      }
+    });
+  }
 };
 
 await copy();
--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,27 @@
+import * as fs from "fs";
+import * as path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const create = async () => {
-    // Write your code here 
+  const filePath = path.join(__dirname, "files", "fresh.txt");
+
+  fs.access(filePath, (err) => {
+    if (!err) {
+      throw new Error("FS error");
+    } else {
+      const content = "I am young and beautiful";
+      fs.writeFile(filePath, content, (err) => {
+        if (err) {
+          console.error(err.message);
+        } else {
+          console.log("The file is written.");
+        }
+      });
+    }
+  });
 };
 
 await create();
