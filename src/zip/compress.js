24471e3fa166d7ff1856690f1223e4f3f22dad7d
Kubatbekkk <kubineccastro@gmail.com>
2023-11-18 21:00:21 +0600

    feat: args, env, list, calHash, cjsToEsm, streams

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,19 @@
+import * as fs from "fs";
+import { pipeline as callBackPipeline } from "stream";
+import { promisify } from "util";
+import zlib from "zlib";
+const pipeline = promisify(callBackPipeline);
+
 const compress = async () => {
-    // Write your code here 
+  // Write your code here
+  const gzip = zlib.createGzip();
+  const source = fs.createReadStream("./files/fileToCOmpress.txt");
+  const destination = fs.createWriteStream("./files/archive.gz");
+  try {
+    await pipeline(source, gzip, destination);
+  } catch (error) {
+    console.error(error);
+  }
 };
 
-await compress();
+compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,19 @@
+import * as fs from "fs";
+import { pipeline as cbPipeline } from "stream";
+import { promisify } from "util";
+import { createUnzip } from "zlib";
+const pipeline = promisify(cbPipeline);
+
 const decompress = async () => {
-    // Write your code here 
+  // Write your code here
+  const unzip = createUnzip();
+  const source = fs.createReadStream("./files/archive.gz");
+  const destination = fs.createWriteStream("./files/fileToCompress.txt");
+  try {
+    await pipeline(source, unzip, destination);
+  } catch (error) {
+    console.error(error);
+  }
 };
 
-await decompress();
+decompress();
