a08f700fa39b33a94a76e74772beb44e2cd5fea1
Kubatbekkk <kubineccastro@gmail.com>
2023-11-19 11:06:01 +0600

    feat: function with Workers

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,39 @@
+import { Worker, isMainThread, parentPort, workerData } from "worker_threads";
+import * as OS from "os";
+
 const performCalculations = async () => {
-    // Write your code here
+  // Write your code here
+  if (isMainThread) {
+    const CPUNumbers = OS.cpus().length;
+    let workersCompleted = 0;
+    let results = [];
+
+    for (let i = 0; i < CPUNumbers; i++) {
+      const worker = new Worker("./worker.js");
+      const n = 10 + i;
+      worker.postMessage(n);
+
+      worker.on("message", (result) => {
+        results[i] = result;
+        workersCompleted++;
+
+        if (workersCompleted === CPUNumbers) {
+          console.log(results);
+        }
+      });
+
+      worker.on("error", () => {
+        results[i] = { status: "error", data: null };
+        workersCompleted++;
+      });
+
+      worker.on("exit", (code) => {
+        if (code != 0) {
+          console.log(`Worker stopped with exit code ${code}`);
+        }
+      });
+    }
+  }
 };
 
 await performCalculations();
